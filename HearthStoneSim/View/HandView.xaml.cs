aef31acd89a24e56e5d31ff3485f6cf1d09852c8
merd1978 <nss1978@gmail.com>
2016-12-27 14:56:18 +0300

    27/12/2016

---
+++ HearthStoneSim/View/HandView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace HearthStoneSim.View
+{
+    /// <summary>
+    /// Description for HandView.
+    /// </summary>
+    public partial class HandView : Window
+    {
+        /// <summary>
+        /// Initializes a new instance of the HandView class.
+        /// </summary>
+        public HandView()
+        {
+            InitializeComponent();
+        }
+    }
+}
