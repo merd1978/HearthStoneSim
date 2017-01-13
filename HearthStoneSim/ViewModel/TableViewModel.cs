c545751f30a467d0594605d5f91f6c1027109cdb
merd1978 <nss1978@gmail.com>
2017-01-13 11:44:01 +0300

    added Table collection

---
+++ HearthStoneSim/ViewModel/TableViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.ObjectModel;
+using GalaSoft.MvvmLight;
+using HearthStoneSim.Model;
+
+namespace HearthStoneSim.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that a View can data bind to.
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class TableViewModel : ViewModelBase
+    {
+        public ObservableCollection<Card> Cards { get; private set; }
+        /// <summary>
+        /// Initializes a new instance of the TableViewModel class.
+        /// </summary>
+        public TableViewModel()
+        {
+            Cards = new ObservableCollection<Card> { new Card(), new Card(), new Card() };
+        }
+    }
+}
