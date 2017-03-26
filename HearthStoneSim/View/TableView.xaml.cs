ab83485811de8c1898e36431307c466addf157ba
merlin <merlin@MERCED>
2017-03-27 00:51:16 +0300

    up

--- HearthStoneSim/View/TableView.xaml.cs
+++ HearthStoneSim/View/TableView.xaml.cs
@@ -14,21 +14,20 @@ namespace HearthStoneSim.View
     public partial class TableView : UserControl
     {
         //private TargetPointerAdorner _targetPointer;
-        private static TargetPointerAdorner _DragAdorner;
+        private static TargetPointerAdorner _dragAdorner;
         private static TargetPointerAdorner DragAdorner
         {
-            get { return _DragAdorner; }
+            get { return _dragAdorner; }
             set
             {
-                _DragAdorner?.Detatch();
-                _DragAdorner = value;
+                _dragAdorner?.Detatch();
+                _dragAdorner = value;
             }
         }
         private static DragInfo m_DragInfo;
         private static bool m_DragInProgress;
         private static Point _adornerPos;
         private static Size _adornerSize;
-        private Point _anchorPoint;
         public static IDropTarget DefaultDropHandler { get; } = new DefaultDropHandler();
         public static IDragSource DefaultDragHandler { get; } = new DefaultDragHandler();
 
@@ -41,13 +40,6 @@ namespace HearthStoneSim.View
         {
         }
                 
-        private void OnMouseDown(object sender, MouseButtonEventArgs e)
-        {
-            //var anchor = e.GetPosition((IInputElement)sender);
-            //_targetPointer.CaptureMouse();
-            //_targetPointer.StartSelection(anchor);
-        }
-
         private void CreateDragAdorner(DropInfo dropInfo)
         {
             var rootElement = RootElementFinder.FindRoot(dropInfo.VisualTarget ?? m_DragInfo.VisualSource);
@@ -82,13 +74,12 @@ namespace HearthStoneSim.View
                 m_DragInfo = null;
                 return;
             }
-
-            _anchorPoint = elementPosition;
         }
 
         private void DragSourceOnMouseMove(object sender, MouseEventArgs e)
         {
-            if (m_DragInfo != null && !m_DragInProgress)
+            if (m_DragInfo == null) return;
+            if (!m_DragInProgress)
             {
 
                 // the start from the source
@@ -117,6 +108,63 @@ namespace HearthStoneSim.View
                     {
                         dragHandler.StartDrag(m_DragInfo);
                     }
+                }
+
+                m_DragInProgress = true;
+            }
+            else
+            {
+                //var dropInfo = new DropInfo(sender, e, m_DragInfo);
+                if (DragAdorner == null && m_DragInfo != null)
+                {
+                    CreateDragAdorner(dropInfo);
+                }
+
+                if (DragAdorner != null)
+                {
+                    var tempAdornerPos = e.GetPosition(DragAdorner.AdornedElement);
+
+                    if (tempAdornerPos.X >= 0 && tempAdornerPos.Y >= 0)
+                    {
+                        _adornerPos = tempAdornerPos;
+                    }
+
+                    // Fixed the flickering adorner - Size changes to zero 'randomly'...?
+                    if (DragAdorner.RenderSize.Width > 0 && DragAdorner.RenderSize.Height > 0)
+                    {
+                        _adornerSize = DragAdorner.RenderSize;
+                    }
+
+                    if (m_DragInfo != null)
+                    {
+                        // move the adorner
+                        var offsetX = _adornerSize.Width;
+                        var offsetY = _adornerSize.Height;
+                        _adornerPos.Offset(offsetX, offsetY);
+                        var maxAdornerPosX = DragAdorner.AdornedElement.RenderSize.Width;
+                        var adornerPosRightX = (_adornerPos.X + _adornerSize.Width);
+                        if (adornerPosRightX > maxAdornerPosX)
+                        {
+                            _adornerPos.Offset(-adornerPosRightX + maxAdornerPosX, 0);
+                        }
+                        if (_adornerPos.Y < 0)
+                        {
+                            _adornerPos.Y = 0;
+                        }
+                    }
+
+                    DragAdorner.DrawSelection(sender, e, _anchorPoint);
+
+                    DragAdorner.MousePosition = _adornerPos;
+                    DragAdorner.InvalidateVisual();
+                }
+
+                e.Effects = dropInfo.Effects;
+                e.Handled = !dropInfo.NotHandled;
+
+                if (!dropInfo.IsSameDragDropContextAsSource)
+                {
+                    e.Effects = DragDropEffects.None;
                 }
             }
         }
